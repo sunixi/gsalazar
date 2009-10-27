7cb66bec35a9580c29ace7f33d15a6e552a1d9e1
guillesalazar <guillesalazar@12a2e966-8dd8-11de-b94f-a9793fafe3e9>
2009-10-27 20:33:57 +0000

    Se elimina la parte de query daos, y algunos servicios de la arquitectura en gsalazar.

--- trunk/gsalazar-core/src/main/test/com/angel/gsalazar/services/BasicQueriesTestCase.java
+++ trunk/gsalazar-core/src/main/test/com/angel/gsalazar/services/BasicQueriesTestCase.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import com.angel.dao.generic.query.clauses.FromClause;
+import com.angel.dao.generic.query.clauses.OrderByClause;
 import com.angel.dao.generic.query.clauses.SelectClause;
 import com.angel.dao.generic.query.clauses.WhereClause;
 
@@ -28,7 +29,7 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 	public void testFromWithPrefix(){
 		String expectedQuery = "from eg.Cat";
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
-		fromClause.add("eg.Cat");
+		fromClause.from("eg.Cat");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
 		assertEquals("Query should be equals.", expectedQuery, query);
@@ -44,7 +45,7 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 	public void testFromWithoutPrefix(){
 		String expectedQuery = "from Cat";
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
-		fromClause.add("Cat");
+		fromClause.from("Cat");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
 		assertEquals("Query should be equals.", expectedQuery, query);
@@ -60,7 +61,7 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 	public void testFromWithAlias(){
 		String expectedQuery = "from Cat cat";
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
-		fromClause.add("Cat", "cat");
+		fromClause.from("Cat", "cat");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
 		assertEquals("Query should be equals.", expectedQuery, query);
@@ -77,8 +78,8 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 		String expectedQuery = "from Formula, Parameter";
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
 		fromClause
-			.add("Formula")
-			.add("Parameter");
+			.from("Formula")
+			.from("Parameter");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
 		assertEquals("Query should be equals.", expectedQuery, query);
@@ -95,8 +96,8 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 		String expectedQuery = "from Formula form, Parameter param";
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
 		fromClause
-			.add("Formula","form")
-			.add("Parameter", "param");
+			.from("Formula","form")
+			.from("Parameter", "param");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
 		assertEquals("Query should be equals.", expectedQuery, query);
@@ -114,7 +115,7 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 		String expectedQuery = "from Cat cat left join cat.mate.kittens kittens";
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
 		fromClause
-			.add("Cat","cat")
+			.from("Cat","cat")
 			.leftJoin("cat.mate.kittens", "kittens");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
@@ -132,7 +133,7 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 		String expectedQuery = "from Formula form full join form.parameter param";
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
 		fromClause
-			.add("Formula","form")
+			.from("Formula","form")
 			.fullJoin("form.parameter", "param");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
@@ -150,7 +151,7 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 		String expectedQuery = "from Cat cat inner join cat.mate mate left join cat.kittens kitten";
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
 		fromClause
-			.add("Cat","cat")
+			.from("Cat","cat")
 			.innerJoin("cat.mate", "mate")
 			.leftJoin("cat.kittens", "kitten");
 
@@ -168,7 +169,7 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 	public void testFromWithLikeCondition(){
 		String expectedQuery = "from Cat cat where cat.mate.name like '%:param_0%'";
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
-		fromClause.add("Cat","cat");
+		fromClause.from("Cat","cat");
 		WhereClause whereClause = this.getQueryBuilder().getWhereClause();
 		whereClause.like("cat.mate.name", "s");
 
@@ -189,7 +190,7 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 		selectClause.addAliased("cat", "mate");
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
 		fromClause
-			.add("Cat","cat");
+			.from("Cat","cat");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
 		assertEquals("Query should be equals.", expectedQuery, query);
@@ -208,7 +209,7 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 		selectClause.add("mate");
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
 		fromClause
-			.add("Cat","cat")
+			.from("Cat","cat")
 			.innerJoin("cat.mate", "mate");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
@@ -228,12 +229,128 @@ public class BasicQueriesTestCase extends BaseQueriesTestCase{
 		selectClause.addAliased("cat", "name");
 		FromClause fromClause = this.getQueryBuilder().getFromClause();
 		fromClause
-			.add("DomesticCat","cat");
+			.from("DomesticCat","cat");
 		WhereClause whereClause = this.getQueryBuilder().getWhereClause();
 		whereClause.likeRight("cat", "name", "fri");
 
 		String query = this.getQueryBuilder().buildQuery().getQuery();
 		assertEquals("Query should be equals.", expectedQuery, query);
 	}
+
+	/**
+	 * Create a query like
+	 * <code>
+	 * 	from Document fetch all properties
+	 * </code>
+	 */
+	@Test
+	public void testBasicSelectFetchAllProperties(){
+		String expectedQuery = "from Document fetch all properties";
+
+		FromClause fromClause = this.getQueryBuilder().getFromClause();
+		fromClause
+			.fromFetchProperties("Document");
+
+		String query = this.getQueryBuilder().buildQuery().getQuery();
+		assertEquals("Query should be equals.", expectedQuery, query);
+	}
 	
+	/**
+	 * Create a query like
+	 * <code>
+	 * 	from Document fetch all properties order by name
+	 * </code>
+	 */
+	@Test
+	public void testBasicSelectFetchAllPropertiesOrderByName(){
+		String expectedQuery = "from Document fetch all properties order by name desc";
+
+		FromClause fromClause = this.getQueryBuilder().getFromClause();
+		fromClause.fromFetchProperties("Document");
+		OrderByClause orderByClause = this.getQueryBuilder().getOrderByClause();
+		orderByClause.desc("name");
+
+		String query = this.getQueryBuilder().buildQuery().getQuery();
+		assertEquals("Query should be equals.", expectedQuery, query);
+	}
+	
+	/**
+	 * Create a query like
+	 * <code>
+	 * 	from Cat cat inner join fetch cat.mate
+	 * </code>
+	 */
+	@Test
+	public void testBasicInnerJoinWithFecth(){
+		String expectedQuery = "from Cat cat inner join fetch cat.mate";
+
+		FromClause fromClause = this.getQueryBuilder().getFromClause();
+		fromClause
+			.from("Cat", "cat")
+			.innerJoinFetch("cat.mate");
+
+		String query = this.getQueryBuilder().buildQuery().getQuery();
+		assertEquals("Query should be equals.", expectedQuery, query);
+	}
+	
+	/**
+	 * Create a query like
+	 * <code>
+	 * 	from Cat cat inner join fetch cat.mate left join fetch cat.kittens
+	 * </code>
+	 */
+	@Test
+	public void testBasic2JoinsWithFecth(){
+		String expectedQuery = "from Cat cat inner join fetch cat.mate left join fetch cat.kittens";
+
+		FromClause fromClause = this.getQueryBuilder().getFromClause();
+		fromClause
+			.from("Cat", "cat")
+			.innerJoinFetch("cat.mate")
+			.leftJoinFetch("cat.kittens");
+
+		String query = this.getQueryBuilder().buildQuery().getQuery();
+		assertEquals("Query should be equals.", expectedQuery, query);
+	}
+	
+	/**
+	 * Create a query like
+	 * <code>
+	 * 	from Document doc fetch all properties where doc.name like '%cats%'
+	 * </code>
+	 */
+	@Test
+	public void testBasicFecthAllPropertiesWithWhereConditions(){
+		String expectedQuery = "from Document doc fetch all properties where doc.name like '%:param_0%'";
+
+		FromClause fromClause = this.getQueryBuilder().getFromClause();
+		fromClause.fromFetchProperties("Document", "doc");
+		WhereClause whereClause = this.getQueryBuilder().getWhereClause();
+		whereClause.like("doc", "name", "cats");
+
+		String query = this.getQueryBuilder().buildQuery().getQuery();
+		assertEquals("Query should be equals.", expectedQuery, query);
+	}
+
+	
+	/**
+	 * Create a query like
+	 * <code>
+	 * 	from Cat cat inner join fetch cat.mate left join fetch cat.kittens child left join fetch child.kittens
+	 * </code>
+	 */
+	@Test
+	public void testBasicJoinsWithFecthWithWhereConditions(){
+		String expectedQuery = "from Cat cat inner join fetch cat.mate left join fetch cat.kittens child left join fetch child.kittens";
+
+		FromClause fromClause = this.getQueryBuilder().getFromClause();
+		fromClause
+			.from("Cat", "cat")
+			.innerJoinFetch("cat.mate")
+			.leftJoinFetch("cat.kittens", "child")
+			.leftJoinFetch("child.kittens");
+
+		String query = this.getQueryBuilder().buildQuery().getQuery();
+		assertEquals("Query should be equals.", expectedQuery, query);
+	}
 }
